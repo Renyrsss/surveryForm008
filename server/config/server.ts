b427d5dd8447ff2ba892058799c485e4b09fb777
renys <renys_northside@mail.ru>
2025-09-04 15:01:10 +0500

    add cors2

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,11 +1,19 @@
+import fs from "fs";
 export default ({ env }) => ({
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
-    app: {
-        keys: env.array("APP_KEYS"),
-    },
-    url: "https://phone-marc-broadway-medium.trycloudflare.com/", // 👈 сюда твой cloudflare-домен
+    url: "https://localhost:1339",
+    app: { keys: env.array("APP_KEYS") }, // 👈 сюда твой cloudflare-домен
     allowedHosts: [
         ".trycloudflare.com", // 👈 можно сразу разрешить все *.trycloudflare.com
     ],
+
+    ssl: {
+        key: fs.readFileSync(
+            "/home/hdadmin/nnmcsigndoc/client/localhost+2-key.pem"
+        ),
+        cert: fs.readFileSync(
+            "/home/hdadmin/nnmcsigndoc/client/localhost+2.pem"
+        ),
+    },
 });
