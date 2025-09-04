181659d76feec056d7b9739f81f846cb69415279
renys <renys_northside@mail.ru>
2025-09-04 14:55:10 +0500

    add cors

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,21 +1,19 @@
-export default [
-    "strapi::logger",
+module.exports = ({ env }) => [
     "strapi::errors",
     "strapi::security",
-    "strapi::cors",
+    {
+        name: "strapi::cors",
+        config: {
+            origin: ["https://localhost:4000"], // фронт, который будет обращаться
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            credentials: true,
+        },
+    },
     "strapi::poweredBy",
+    "strapi::logger",
     "strapi::query",
     "strapi::body",
     "strapi::session",
     "strapi::favicon",
     "strapi::public",
-    {
-        name: "strapi::cors",
-        config: {
-            enabled: true,
-            origin: ["https://192.168.101.25:4000"], // твой фронт
-            headers: "*",
-            methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        },
-    },
 ];
