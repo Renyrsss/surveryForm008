b8ab29548bef6da2e0f38d8d644e408d600dba73
renys <renys_northside@mail.ru>
2025-09-11 11:03:19 +0500

    edit server.js

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,19 +1,8 @@
-import fs from "fs";
 export default ({ env }) => ({
     host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 1337),
-    url: "https://localhost:1339",
-    app: { keys: env.array("APP_KEYS") }, // 👈 сюда твой cloudflare-домен
-    allowedHosts: [
-        ".trycloudflare.com", // 👈 можно сразу разрешить все *.trycloudflare.com
-    ],
-
-    ssl: {
-        key: fs.readFileSync(
-            "/home/hdadmin/nnmcsigndoc/client/localhost+2-key.pem"
-        ),
-        cert: fs.readFileSync(
-            "/home/hdadmin/nnmcsigndoc/client/localhost+2.pem"
-        ),
+    port: env.int("PORT", 1339),
+    app: {
+        keys: env.array("APP_KEYS"),
     },
 });
+//asdfasdfg
